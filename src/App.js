01b0958ccd5c7b50877289cab1e91ac51f4e91f6
Hüseyin Eryilmaz <45396546+huseyineryilmaz@users.noreply.github.com>
2022-07-31 16:10:04 +0300

    Add files via upload
    initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {useSelector} from 'react-redux';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import CartScreen from './screens/CartScreen';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+function App() {
+    const cart = useSelector((state) => state.cart);
+    const {cartItems} = cart;
+    return (
+        <Router>
+            <div className='grid-container'>
+                <header className='row'>
+                    <div>
+                        <Link className='brand' to='/'>amazona</Link>
+                    </div>
+                    <div>
+                        <Link to='/cart'>
+                            Cart
+                            {cartItems.length > 0 && (
+                                <span className='badge'>{cartItems.length}</span>
+                            )}
+                        </Link>
+                        <Link to='/signin'>Sign In</Link>
+                    </div>
+                </header>
+                <main>
+                    <Routes>
+                        <Route path='/cart' element={<CartScreen />}></Route>
+                        <Route path='/product/:id' element={<ProductScreen />} ></Route>
+                        <Route path='/' element={<HomeScreen />} exact> </Route>
+                    </Routes>
+                </main>
+                <footer className='row center'>All right reserved</footer>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Lemon Slim Jean',
+            category: 'Jean',
+            image: '/images/p1.jpg',
+            price: 250,
+            brand: 'Lemon',
+            rating: 4.5,
+            numReviews: 23,
+            countInStock: 10,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula dui eget tortor fringilla tristique. Proin aliquet libero ac tellus hendrerit ultricies.'
+        },
+        {
+            _id: '2',
+            name: 'Lemon Slim Jean',
+            category: 'Jean',
+            image: '/images/p1.jpg',
+            price: 354,
+            brand: 'Lemon',
+            rating: 3.5,
+            numReviews: 76,
+            countInStock: 10,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula dui eget tortor fringilla tristique. Proin aliquet libero ac tellus hendrerit ultricies.'
+        },
+        {
+            _id: '3',
+            name: 'Lemon Slim Jean',
+            category: 'Jean',
+            image: '/images/p1.jpg',
+            price: 586,
+            brand: 'Lemon',
+            rating: 2.5,
+            numReviews: 54,
+            countInStock: 10,
+            description: 'Lorem ipsum dolor sit amet.'
+        },
+        {
+            _id: '4',
+            name: 'Lemon Slim Jean',
+            category: 'Jean',
+            image: '/images/p1.jpg',
+            price: 652,
+            brand: 'Lemon',
+            rating: 4.5,
+            numReviews: 13,
+            countInStock: 10,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula dui eget tortor fringilla tristique. Proin aliquet libero ac tellus hendrerit ultricies.'
+        },
+        {
+            _id: '5',
+            name: 'Lemon Slim Jean',
+            category: 'Jean',
+            image: '/images/p1.jpg',
+            price: 145,
+            brand: 'Lemon',
+            rating: 4,
+            numReviews: 23,
+            countInStock: 10,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula dui eget tortor fringilla tristique. Proin aliquet libero ac tellus hendrerit ultricies.'
+        },
+        {
+            _id: '6',
+            name: 'Lemon Slim Jean',
+            category: 'Jean',
+            image: '/images/p1.jpg',
+            price: 185,
+            brand: 'Lemon',
+            rating: 3,
+            numReviews: 36,
+            countInStock: 10,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula dui eget tortor fringilla tristique. Proin aliquet libero ac tellus hendrerit ultricies.'
+        }
+    ]
+};
+
+export default data;
